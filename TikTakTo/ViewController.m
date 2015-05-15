4670a6a23a1dc3084308b590f66f6f2a22122df8
Alex <alexsantorineos@gmail.com>
2015-05-14 19:39:08 -0500

    fdgdg

--- TikTakTo/ViewController.m
+++ TikTakTo/ViewController.m
@@ -36,5 +36,46 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+- (IBAction)onGesture:(UITapGestureRecognizer *)sender {
+
+// this variable captures the tap gesture
+    CGPoint point = [sender locationInView:self.view];
+
+    if (CGRectContainsPoint(self.labelOne.frame, point)) {
+        NSLog(@"labelOne");
+    }
+    if (CGRectContainsPoint(self.labelTwo.frame, point)) {
+        NSLog(@"labelTwo");
+    }
+
+    if (CGRectContainsPoint(self.labelThree.frame, point)) {
+        NSLog(@"labelThree");
+    }
+
+    if (CGRectContainsPoint(self.labelFour.frame, point)) {
+        NSLog(@"label4");
+    }
+
+    if (CGRectContainsPoint(self.labelFive.frame, point)) {
+        NSLog(@"label5");
+    }
+
+    if (CGRectContainsPoint(self.labelSix.frame, point)) {
+        NSLog(@"label6");
+    }
+
+    if (CGRectContainsPoint(self.labelSeven.frame, point)) {
+        NSLog(@"label7");
+    }
+
+    if (CGRectContainsPoint(self.labelEight.frame, point)) {
+        NSLog(@"label8");
+    }
+
+    if (CGRectContainsPoint(self.labelNine.frame, point)) {
+        NSLog(@"label9");
+    }
+
+}
 
 @end
