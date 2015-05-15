724d925209c271ffa04a6284c91a8778913d2438
Alex <alexsantorineos@gmail.com>
2015-05-15 09:01:51 -0500

    ddfs

--- TikTakTo/ViewController.m
+++ TikTakTo/ViewController.m
@@ -21,6 +21,9 @@
 @property (weak, nonatomic) IBOutlet UILabel *labelNine;
 @property (weak, nonatomic) IBOutlet UILabel *whichPlayerLabel;
 
+@property BOOL currentPlayer;
+@property NSArray *labels;
+
 
 
 @end
@@ -30,58 +33,116 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.whichPlayerLabel.text = @"player1";
+
+
+    self.labels = @[self.labelOne, self.labelTwo,self.labelThree, self.labelFour,self.labelFive, self.labelSix, self.labelSeven, self.labelEight, self.labelNine];
+
+    //0 is player1. 1 is player 2
+    self.currentPlayer = 0;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)updateLabel:(UILabel *)currentLabel{
+
+    if ([currentLabel.text isEqualToString:@""]) {
+
+
+        if (self.currentPlayer == 0) {
+         currentLabel.text = @"x";
+            self.currentPlayer = 1;
+        }
+        else if (self.currentPlayer == 1){
+
+        currentLabel.text = @"o";
+            self.currentPlayer = 0;
+        }
+
+    }
+
+
+
+
+
+
+
+
 }
 - (IBAction)onGesture:(UITapGestureRecognizer *)sender {
 
 // this variable captures the tap gesture
     CGPoint point = [sender locationInView:self.view];
+    for (UILabel *label in self.labels) {
 
-    if (CGRectContainsPoint(self.labelOne.frame, point)) {
-        NSLog(@"labelOne");
+        if (CGRectContainsPoint(label.frame, point)) {
+            NSLog(@"tag ==  %li",(long)label.tag);
 
+            [self updateLabel:label];
 
-
+            
         }
 
-    if (CGRectContainsPoint(self.labelTwo.frame, point)) {
-        NSLog(@"labelTwo");
-
-
-
     }
 
-    if (CGRectContainsPoint(self.labelThree.frame, point)) {
-        NSLog(@"labelThree");
-    }
 
-    if (CGRectContainsPoint(self.labelFour.frame, point)) {
-        NSLog(@"label4");
-    }
 
-    if (CGRectContainsPoint(self.labelFive.frame, point)) {
-        NSLog(@"label5");
-    }
 
-    if (CGRectContainsPoint(self.labelSix.frame, point)) {
-        NSLog(@"label6");
-    }
 
-    if (CGRectContainsPoint(self.labelSeven.frame, point)) {
-        NSLog(@"label7");
-    }
-
-    if (CGRectContainsPoint(self.labelEight.frame, point)) {
-        NSLog(@"label8");
-    }
-
-    if (CGRectContainsPoint(self.labelNine.frame, point)) {
-        NSLog(@"label9");
-    }
+//    if (CGRectContainsPoint(self.labelOne.frame, point)) {
+//        NSLog(@"labelOne");
+//
+//        [self updateLabel:self.labelOne];
+//
+//
+//        }
+//
+//    if (CGRectContainsPoint(self.labelTwo.frame, point)) {
+//        NSLog(@"labelTwo");
+//
+//        [self updateLabel:self.labelTwo];
+//
+//
+//    }
+//
+//    if (CGRectContainsPoint(self.labelThree.frame, point)) {
+//        NSLog(@"labelThree");
+//        [self updateLabel:self.labelTwo];
+//
+//    }
+//
+//    if (CGRectContainsPoint(self.labelFour.frame, point)) {
+//        NSLog(@"label4");
+//        [self updateLabel:self.labelTwo];
+//
+//    }
+//
+//    if (CGRectContainsPoint(self.labelFive.frame, point)) {
+//        NSLog(@"label5");
+//        [self updateLabel:self.labelTwo];
+//
+//    }
+//
+//    if (CGRectContainsPoint(self.labelSix.frame, point)) {
+//        NSLog(@"label6");
+//        [self updateLabel:self.labelTwo];
+//
+//    }
+//
+//    if (CGRectContainsPoint(self.labelSeven.frame, point)) {
+//        NSLog(@"label7");
+//        [self updateLabel:self.labelTwo];
+//
+//    }
+//
+//    if (CGRectContainsPoint(self.labelEight.frame, point)) {
+//        NSLog(@"label8");
+//        [self updateLabel:self.labelTwo];
+//
+//    }
+//
+//    if (CGRectContainsPoint(self.labelNine.frame, point)) {
+//        NSLog(@"label9");
+//        [self updateLabel:self.labelTwo];
+//
+//    }
 
 }
 
